ed65f8d686edab124f1e29f4b50cb7ae7372557c
Giorgio Daniele <giorgiodaniele15@gmail.com>
2024-12-28 20:59:38 +0100

    Introduction of folder for storing the result. UDP to do

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+/* main.rs */
+
 pub mod tcp;
 
+use std::{fs::{self}, io::{self}, path::Path};
 use pcap::Capture;
-use tcp::process_trace;
+use tcp::{process_trace, save};
 use clap::Parser;
-
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -13,30 +15,39 @@ struct Args {
 
 }
 
-fn main() {
+fn main() -> io::Result<()> {
+    // Parse command-line arguments
     let args = Args::parse();
 
     // The input trace
-    let input = args.input;
+    let input = &args.input;
 
-    match Capture::from_file(input) {
-        Ok(capture) => {
-            let connections = process_trace(capture);
+    // Define the output folder path
+    let output_folder = format!("{}.out", input);
 
-            // Print the header
-            println!("c_ip c_port s_ip s_port \
-            c_packs c_bytes c_segms c_acks c_syns c_fins c_rsts c_pshs c_urgs c_ts c_te \
-            s_packs s_bytes s_segms s_acks s_syns s_fins s_rsts s_pshs s_urgs s_ts s_te");
+    // Create the folder path
+    let folder_path = Path::new(&output_folder);
 
-            // Print the connections
-            for (connection, (client, server)) in connections {
-                println!("{} {} {}", connection, client, server)
-            }
-        }
-        Err(error) => {
-            println!("[ERROR]: {}", error);
-            std::process::exit(1);
-        }
+    // Create the directory if it does not exist
+    fs::create_dir_all(folder_path).expect("[ERROR]: Failed to create output folder");
+
+    // Define the file paths where save the results
+    let log_tcp = folder_path.join("tcp_flows.csv");
+
+    // Open the capture file
+    let capture = Capture::from_file(input).unwrap_or_else(|error| {
+        eprintln!("[ERROR]: {}", error);
+        std::process::exit(2);
+    });
+
+    // Process the TCP connections from the capture
+    let connections = process_trace(capture);
+
+    // Save the TCP connections
+    match save(log_tcp, connections) {
+        Ok(_) => println!("[MESSAGE]: TCP connections have been reconstructed"),
+        Err(error) => eprintln!("[ERROR]: {}", error),
     }
-    std::process::exit(0);
+
+    Ok(())
 }
